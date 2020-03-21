af91d4487ae4e384cd88b5a90cf093a3c14dbb59
Paty Alves <patriciaalves10@gmail.com>
2020-03-20 21:26:30 -0300

    created a Layout component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return <div>Learn React</div>;
+  return (
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
